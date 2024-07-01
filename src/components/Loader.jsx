67c1879dfbb8bc6ad084b6025c570ad6fdb3ba91
TrustSign123 <developer.trustsign@gmail.com>
2024-07-02 00:18:56 +0530

    loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import loading from "../../public/loader.gif";
+import Image from "next/image";
 
 const Loader = () => {
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen">
-      <div className="animate-spin rounded-full h-12 w-12 border-4 border-black border-t-transparent" />
-      <p className="mt-4 text-muted-foreground">Loading games...</p>
+      <Image src={loading} alt="loading" height={100} width={100} />
+      <p className="mt-4 font-semibold text-xl bg-gradient-to-r from-gray-900 to-slate-300 text-transparent bg-clip-text">
+        Loading Games...
+      </p>
     </div>
   );
 };
